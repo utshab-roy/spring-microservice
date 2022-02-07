8d07d099d1aa36a4fd884097b798b0a1bf0087ba
Utshab Roy <utshab.roy@gmail.com>
2022-02-07 16:58:51 +0900

    postgre db created with docker-compose, Model, Controller, Service and Request created for customer

---
+++ customer/src/main/java/com/utshab/customer/CustomerService.java
@@ -0,0 +1,17 @@
+package com.utshab.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        // TODO: check the email is valid and not taken
+        // TODO: if valid then store the customer in database
+    }
+}
